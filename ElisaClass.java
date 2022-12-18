88f62f82caefd4d02cf422f345958df9605a6614
FerideMMB <120869082+FerideMMB@users.noreply.github.com>
2022-12-18 22:30:23 +0000

    Update ElisaClass.java

--- ElisaClass.java
+++ ElisaClass.java
@@ -1,5 +1,6 @@
 public class ElisaClass {
     public static void main(String[] args) {
         System.out.println("Elisa dan merhaba!");
+        System.out.println("Elisa dan Batuya merhaba!");
     }
 }
